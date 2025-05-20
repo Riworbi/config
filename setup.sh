72abacbe8f287b3623e298da8d8658a3fe32bbdd
Riworbi <riworbix@gmail.com>
2025-05-20 22:27:02 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install fish shell
+sudo add-apt-repository ppa:fish-shell/release-4
+sudo apt update
+sudo apt install fish
+
+echo /usr/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+
+
+# Install starship for fish shell
+curl -sS https://starship.rs/install.sh | sh
+echo "starship init fish | source" | sudo tee -a ~/.config/fish/config.fish
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
