da0c001b525ce3b35c948d3f5d7bd67dba6535f1
Riworbi <riworbix@gmail.com>
2025-05-21 20:11:19 +0200

    script update

--- setup.sh
+++ setup.sh
@@ -13,3 +13,10 @@ chsh -s /usr/local/bin/fish
 curl -sS https://starship.rs/install.sh | sh
 echo "starship init fish | source" | sudo tee -a ~/.config/fish/config.fish
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
+
+echo "fish_vi_key_bindings" | sudo tee -a ~/.config/fish/config.fish
+
+sudo snap install nvim --classic
+
+alias --save v_fish='cd ~/.config/fish && vim config.fish'
+alias --save vi='nvim .'
