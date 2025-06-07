77eeaa0f44f098c68963241b1209cba0fb423d4b
Riworbi <riworbix@gmail.com>
2025-06-07 12:08:03 +0200

    fish fix

--- setup.sh
+++ setup.sh
@@ -9,17 +9,23 @@ sudo apt install fish
 # Set fish shell as default
 
 if grep -q "/usr/bin/fish" /etc/shells; then
+	echo "Fish default set"
 else
 	echo /usr/bin/fish | sudo tee -a /etc/shells
-	chsh -s /usr/local/bin/fish
+	chsh -s /usr/bin/fish
 fi
+
+
+mkdir ~/.config/fish
+touch ~/.config/fish/config.fish
+
 
 # Install starship for fish shell
 curl -sS https://starship.rs/install.sh | sh
 
 if grep -q "starship init fish | source" ~/.config/fish/config.fish; then
+	echo "Starship set"
 else
-
 	echo "starship init fish | source" | sudo tee -a ~/.config/fish/config.fish
 fi
 
@@ -29,14 +35,11 @@ starship preset gruvbox-rainbow -o ~/.config/starship.toml
 
 # - vim mode in fish
 if grep -q "fish_vi_key_bindings" ~/.config/fish/config.fish; then
+	echo "fish Vi mode set"
 else
-
 	echo "fish_vi_key_bindings" | sudo tee -a ~/.config/fish/config.fish
 fi
 
 # Install nvim
 sudo snap install nvim --classic
 
-# Alias
-alias --save v_fish='cd ~/.config/fish && vim config.fish'
-alias --save vi='nvim .'
