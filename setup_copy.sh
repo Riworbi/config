c9b21c4e6f5757b50ae7e506cd14d7c1dc19704b
Riworbi <riworbix@gmail.com>
2025-06-07 18:51:21 +0200

    script_copy.sh almost finished...

--- setup_copy.sh
+++ setup_copy.sh
@@ -12,9 +12,7 @@ print_color(){
 	echo -e "${1}${2}${WHITE}"
 }
 
-# $1 message  $2 destination file
 add_to_end_of_file(){
-
 	if grep -q "$1" "$2"; then
 		print_color ${GREEN} "'$1' is present !"	
 	else
@@ -23,6 +21,22 @@ add_to_end_of_file(){
 	fi
 }
 
+install_package() {
+	read -p "Do you want to install $1 [Y/N]? " answer
+	case "$answer" in
+		[Yy]*)
+			echo "Proceeding with installation..."
+			;;
+		[Nn]*)
+			echo "Exiting without installation."
+			exit 0
+			;;
+		*)
+			echo "Invalid input. Please enter Y or N."
+			install_package  # Prompt again for valid input
+			;;
+	esac
+}
 
 
 fish_shell(){
@@ -47,18 +61,24 @@ check_if_string_exists_in_file() {
 
 # MAIN 
 
-# fish_shell
-check_if_string_exists_in_file $FISH_SHELL '/etc/shells'
+fish_shell
+print_color ${GREEN} "Changing shell to fish"
 chsh -s /usr/bin/fish
 
+mkdir -p ~/.config/fish
+touch ~/.config/fish/config.fish
+
 # Starship:
+install_package "starship"
 curl -sS https://starship.rs/install.sh | sh
 add_to_end_of_file "starship init fish | source" "$FISH_SHELL_CONFIG"
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
 add_to_end_of_file "fish_vi_key_bindings" $FISH_SHELL_CONFIG
 
-touch ~/.tmux.conf
 
+install_package "tmux"
+touch ~/.tmux.conf
 add_to_end_of_file "set-option -g default-shell /usr/bin/fish" $TMUX_CONFIG
 
-#sudo snap install nvim --clasic
+install_package "nvim"
+sudo snap install nvim --classic
