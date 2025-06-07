6d41f3b2b9d7415525c13935460cfe719f016036
Riworbi <riworbix@gmail.com>
2025-06-07 18:29:47 +0200

    additional test script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Install fish shell
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+
+# Fish shell
 sudo add-apt-repository ppa:fish-shell/release-4
 sudo apt update
 sudo apt install fish
 
-
-# Set fish shell as default
+## set  as default
 
 if grep -q "/usr/bin/fish" /etc/shells; then
-	echo "Fish default set"
+	echo -e  "${RED} /usr/bin/fish is present changing shell: "
+	chsh -s /usr/bin/fish
 else
 	echo /usr/bin/fish | sudo tee -a /etc/shells
 	chsh -s /usr/bin/fish
@@ -19,8 +23,7 @@ fi
 mkdir ~/.config/fish
 touch ~/.config/fish/config.fish
 
-
-# Install starship for fish shell
+## Starship for fish shell
 curl -sS https://starship.rs/install.sh | sh
 
 if grep -q "starship init fish | source" ~/.config/fish/config.fish; then
@@ -30,16 +33,23 @@ else
 fi
 
 
-# - gruvbox-rainbow theme
+## Gruvbox-rainbow theme
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
 
-# - vim mode in fish
+## Vim mode in fish
 if grep -q "fish_vi_key_bindings" ~/.config/fish/config.fish; then
 	echo "fish Vi mode set"
 else
 	echo "fish_vi_key_bindings" | sudo tee -a ~/.config/fish/config.fish
 fi
 
-# Install nvim
+# Nvim
 sudo snap install nvim --classic
 
+# Tmux
+if grep -q "set-option -g default-shell /usr/bin/fish" ~/.tmux.conf; then
+
+	echo "Fish shell set as default for Tmux"
+else
+	echo "set-option -g default-shell /usr/bin/fish" > ~/.tmux.conf
+fi 
---
+++ setup_copy.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+WHITE='\e[0m'
+
+FISH_SHELL='/usr/bin/fish'
+FISH_SHELL_CONFIG='/home/user/.config/fish/config.fish'
+TMUX_CONFIG='/home/user/.tmux.conf'
+
+print_color(){
+	echo -e "${1}${2}${WHITE}"
+}
+
+# $1 message  $2 destination file
+add_to_end_of_file(){
+
+	if grep -q "$1" "$2"; then
+		print_color ${GREEN} "'$1' is present !"	
+	else
+		print_color ${RED}  "($1) is not present adding...!"
+		echo "$1" | sudo tee -a "$2"
+	fi
+}
+
+
+
+fish_shell(){
+
+	print_color ${GREEN} 'INSTALL fish_shell'
+	sudo add-apt-repository ppa:fish-shell/release-4
+	sudo apt update
+	sudo apt install fish
+	print_color ${GREEN}  'SHELL INSTALLED'	
+}
+
+check_if_string_exists_in_file() {
+	if grep -q "$1" $2; then
+		print_color ${GREEN}  "($1) is present !"	
+		print_color ${GREEN}  "($1) quitting...  !"	
+		exit
+	else
+		print_color ${RED}  "($1) is not  present !"
+	fi
+}
+
+
+# MAIN 
+
+# fish_shell
+check_if_string_exists_in_file $FISH_SHELL '/etc/shells'
+chsh -s /usr/bin/fish
+
+# Starship:
+curl -sS https://starship.rs/install.sh | sh
+add_to_end_of_file "starship init fish | source" "$FISH_SHELL_CONFIG"
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
+add_to_end_of_file "fish_vi_key_bindings" $FISH_SHELL_CONFIG
+
+touch ~/.tmux.conf
+
+add_to_end_of_file "set-option -g default-shell /usr/bin/fish" $TMUX_CONFIG
+
+#sudo snap install nvim --clasic
